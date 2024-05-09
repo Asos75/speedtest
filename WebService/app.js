ca94bef54c0a72c329f7f39c0bfeda6609277f29
David <rajlic.david@gmail.com>
2024-05-09 09:25:21 +0200

    succesfully connected to mongoDB atlas

--- WebService/app.js
+++ WebService/app.js
@@ -1,89 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-// vključimo mongoose in ga povežemo z MongoDB
-var mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-var mongoDB = "mongodb://localhost:27017/Express";
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://david:nice@host.cqupown.mongodb.net/?retryWrites=true&w=majority&appName=SpeedDB";
 
-// vključimo routerje
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/userRoutes');
-var photosRouter = require('./routes/photoRoutes');
+const mongoose = require('mongoose');
+const User = require('./models/userModel');  // Uvozite model
 
-var app = express();
-
-var cors = require('cors');
-var allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
-app.use(cors({
-  credentials: true,
-  origin: function(origin, callback){
-    // Allow requests with no origin (mobile apps, curl)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin)===-1){
-      var msg = "The CORS policy does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-}));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-/**
- * Vključimo session in connect-mongo.
- * Connect-mongo skrbi, da se session hrani v bazi.
- * Posledično ostanemo prijavljeni, tudi ko spremenimo kodo (restartamo strežnik)
- */
-var session = require('express-session');
-var MongoStore = require('connect-mongo');
-app.use(session({
-  secret: 'work hard',
-  resave: true,
-  saveUninitialized: false,
-  store: MongoStore.create({mongoUrl: mongoDB})
-}));
-//Shranimo sejne spremenljivke v locals
-//Tako lahko do njih dostopamo v vseh view-ih (glej layout.hbs)
-app.use(function (req, res, next) {
-  res.locals.session = req.session;
-  next();
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/photos', photosRouter);
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
-  // render the error page
-  res.status(err.status || 500);
-  //res.render('error');
-  res.json(err);
-});
+async function createUser() {
+  const newUser = new User({
+    username: 'exampleUser',
+    password: 'password123',
+    admin: true
+  });
 
-module.exports = app;
+  try {
+    const result = await newUser.save();
+    console.log('User created:', result);
+  } catch (error) {
+    console.error('Error creating user:', error);
+  }
+}
+
+createUser();
+run().catch(console.dir);
