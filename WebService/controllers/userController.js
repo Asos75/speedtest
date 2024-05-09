2c019cb605a7da4e003816a67aadabd6ae64e686
David <rajlic.david@gmail.com>
2024-05-09 13:18:48 +0200

    hash user password

--- WebService/controllers/userController.js
+++ WebService/controllers/userController.js
@@ -51,14 +51,14 @@ module.exports = {
      * user Controller.create()
      */
     create: function (req, res) {
-        var user  = new UserModel({
+        var user = new UserModel({
 			username : req.body.username,
 			password : req.body.password,
 			measurements : req.body.measurements,
 			admin : req.body.admin
         });
 
-        user .save(function (err, user ) {
+        user.save(function (err, user ) {
             if (err) {
                 return res.status(500).json({
                     message: 'Error when creating user ',
@@ -90,34 +90,34 @@ module.exports = {
                 });
             }
 
-            user .username = req.body.username ? req.body.username : user .username;
-			user .password = req.body.password ? req.body.password : user .password;
-			user .measurements = req.body.measurements ? req.body.measurements : user .measurements;
-			user .admin = req.body.admin ? req.body.admin : user .admin;
+            user.username = req.body.username ? req.body.username : user.username;
+			user.password = req.body.password ? req.body.password : user.password;
+			user.measurements = req.body.measurements ? req.body.measurements : user.measurements;
+			user.admin = req.body.admin ? req.body.admin : user.admin;
 			
-            user .save(function (err, user ) {
+            user.save(function (err, user) {
                 if (err) {
                     return res.status(500).json({
-                        message: 'Error when updating user .',
+                        message: 'Error when updating user.',
                         error: err
                     });
                 }
 
-                return res.json(user );
+                return res.json(user);
             });
         });
     },
 
     /**
-     * user Controller.remove()
+     * userController.remove()
      */
     remove: function (req, res) {
         var id = req.params.id;
 
-        UserModel.findByIdAndRemove(id, function (err, user ) {
+        UserModel.findByIdAndRemove(id, function (err, user) {
             if (err) {
                 return res.status(500).json({
-                    message: 'Error when deleting the user .',
+                    message: 'Error when deleting the user.',
                     error: err
                 });
             }
