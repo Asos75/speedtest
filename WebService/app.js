e61bde4dd2b4cb0fe4bf11c59489ab56b9b1f6b2
David <rajlic.david@gmail.com>
2024-05-12 20:32:40 +0200

    speed34 - added web scokets

--- WebService/app.js
+++ WebService/app.js
@@ -1,17 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const http = require('http');
+const socketIo = require('socket.io');
+
 
 require('dotenv').config();
-
 
 const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.MONGO_URL;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,13 +24,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
@@ -38,33 +36,32 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-// include routes
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/userRoutes');
-var measurementsRouter = require('./routes/measurementRoutes');
-var mobileTowerRouter = require('./routes/mobileTowerRoutes');
-var eventRouter = require('./routes/eventRoutes');
+// Include routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/userRoutes');
+const measurementsRouter = require('./routes/measurementRoutes');
+const mobileTowerRouter = require('./routes/mobileTowerRoutes');
+const eventRouter = require('./routes/eventRoutes');
 
-var app = express()
+const app = express();
 
-var cors = require('cors');
-var allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
-app.use(cors({
-  credentials: true,
-  origin: function(origin, callback){
-    // Allow requests with no origin (mobile apps, curl)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin)===-1){
-      var msg = "The CORS policy does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+const server = http.createServer(app);
+const io = socketIo(server);
 
-app.listen(3000, function() {
-  console.log('Server started on port 3000');
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  // Pošlji sporočilo dobrodošlice samo povezanemu odjemalcu
+  io.emit('welcome', 'Welcome to the server!');
+
+
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
 });
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -72,50 +69,49 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var session = require('express-session');
-var MongoStore = require('connect-mongo');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 app.use(session({
   secret: 'work hard',
   resave: true,
   saveUninitialized: false,
-  store: MongoStore.create({mongoUrl: uri})
+  store: MongoStore.create({ mongoUrl: uri })
 }));
 
 app.use(function (req, res, next) {
   res.locals.session = req.session;
   next();
-});	
-
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/measurements', measurementsRouter);
 app.use('/mobile', mobileTowerRouter);
-app.use('/event', eventRouter)
-
+app.use('/event', eventRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  //res.render('error');
   res.json(err);
 });
 
-
-
-
-const User = require('./models/userModel'); 
+const User = require('./models/userModel');
 const Measurement = require('./models/measurementModel');
+
+
 run().catch(console.dir);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
 module.exports = app;
