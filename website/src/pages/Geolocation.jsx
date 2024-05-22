23e96481ff522db9cafe5fd1c5aa4e97946b6219
DomiiP <95188800+DomiiP@users.noreply.github.com>
2024-05-22 18:31:00 +0200

    Refactored Geolocation.jsx logic with added dates, visual fixes, created a new folder with helpers functions, splitted the code for easier readability and installed new npm packages. It it's not UI / UX optimsied but uploading it because i might down downgrade my react version

--- website/src/pages/Geolocation.jsx
+++ website/src/pages/Geolocation.jsx
@@ -1,163 +1,144 @@
 // Dependencies
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer} from 'react-leaflet';
+import { HeatmapLayer } from 'leaflet-heatmap';
+import { calculateDistance } from '../helpers/helperFunction';
 
 // Styles
 import 'leaflet/dist/leaflet.css';
 import '../styles/Components/Geolocation.css'; 
 
-// Assets
-import { Icon } from 'leaflet';
-import pinIcon from '../assets/Icons/pin.png';
-const customIcon = new Icon({
-  iconUrl: pinIcon,
-  iconSize: [30, 30],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
-
-const MeasurementMarker = ({ measurement, index }) => {
-  // Address state
-  const [address, setAddress] = useState('');
-  // Coordinates are stored in reverse order
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const coordinates = [
-    measurement.location.coordinates[1],
-    measurement.location.coordinates[0]
-  ];
-
-  // FIX ADDRESS FETCHING LATER
-  useEffect(() => {
-    const fetchAddress = async () => {
-      try {
-        const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${coordinates[0]}&lon=${coordinates[1]}`);
-        const data = await response.json();
-        setAddress(data.display_name);
-      } catch (error) {
-        // console.error('Error:', error);
-      }
-    };
-
-    fetchAddress();
-  });
-
-  // Check if coordinates are valid
-  if (coordinates.some(isNaN)) {
-    console.error(`Invalid coordinates for measurement at index ${index}:`, coordinates);
-    return null;
-  }
-
-  return (
-    <Marker key={index} position={coordinates} icon={customIcon}>
-      <Popup>
-        <p><b>{measurement.type}</b></p>
-        <p>Coordinates: <b>{coordinates.join(', ')}</b></p>
-        <p>Address: <b>{address}</b></p>
-        <p>Speed: <b>{measurement.speed.$numberInt ? measurement.speed.$numberInt : 'Currently unvailable'}</b></p>
-        <p>Provider: <b>{measurement.provider}</b></p>
-      </Popup>
-    </Marker>
-  );
-};
-
-const Measurement = ({ measurement, index }) => {
-  // Show details for each measurement
-  const [showDetails, setShowDetails] = useState(false);
-  // Coordinates are stored in reverse order
-  const coordinates = [
-    measurement.location.coordinates[1],
-    measurement.location.coordinates[0]
-  ];
-
-  // Check if coordinates are valid
-  if (coordinates.some(isNaN)) {
-    console.error(`Invalid coordinates for measurement at index ${index}:`, coordinates);
-    return null;
-  }
-
-  return (
-    <div key={index} className="measurement">
-      <p><b>{measurement.type}</b> | Coordinates: <b>{coordinates.join(', ')}</b></p>
-      {showDetails && (
-        <>
-          <p>Speed: <b>{measurement.speed.$numberInt ? measurement.speed.$numberInt : 'Currently unvailable'}</b> | Provider: <b>{measurement.provider}</b></p>
-        </>
-      )}
-      <button onClick={() => setShowDetails(!showDetails)}>{!showDetails ? "Show More" : "Show less"}</button>
-    </div>
-  );
-};
+// SubComponents
+import Measurement from './subComponents/Measurement';
+import MeasurementMarker from './subComponents/MeasurementMarker';
 
 const Geolocation = () => {
   // Measurements
   const [measurements, setMeasurements] = useState([]);
-  const [preloadedMeasurements, setPreloadedMeasurements] = useState([]);
+  const [allMeasurements, setAllMeasurements] = useState([]); // new state for all measurements
+
+  // Filter
+  const [filterType, setFilterType] = useState('dateAsc');
+  const [totalPages, setTotalPages] = useState(1);
+
+  // Layout
+  const [layout, setLayout] = useState('points');
 
   // Pagination
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
 
   // Centering map view
-  // const [mapCenter, setMapCenter] = useState([46.5546, 15.6467]);
   const mapCenter = [46.5546, 15.6467];
 
   // Backend URL
   const backendUrl = process.env.REACT_APP_BACKEND_URL + '/measurements';
 
   // Fetching measurements
-  const fetchMeasurements = async (page) => {
+  const fetchMeasurements = async () => {
     try {
-      const response = await fetch(`${backendUrl}?page=${page}`);
+      const response = await fetch(backendUrl);
       const data = await response.json();
-      const paginatedData = data.slice((page - 1) * itemsPerPage, page * itemsPerPage);
-      return paginatedData;
+      setAllMeasurements(data); // store all measurements
+      setTotalPages(Math.ceil(data.length / itemsPerPage));
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
   // Pagination functions
-  const prevPage = () => currentPage > 1 && setCurrentPage(currentPage - 1);
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   const nextPage = () => {
     setCurrentPage(currentPage + 1);
-    setMeasurements(preloadedMeasurements);
+  };
+
+  const goToPage = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  // Filter function
+  const handleFilter = () => {
+    let filteredMeasurements;
+    switch (filterType) {
+      case 'dateAsc':
+        filteredMeasurements = [...allMeasurements].sort((a, b) => new Date(a.time) - new Date(b.time));
+        break;
+      case 'dateDesc':
+        filteredMeasurements = [...allMeasurements].sort((a, b) => new Date(b.time) - new Date(a.time));
+        break;
+      case 'coordinatesAsc':
+        filteredMeasurements = [...allMeasurements].sort((a, b) => calculateDistance(mapCenter, a.location.coordinates) - calculateDistance(mapCenter, b.location.coordinates));
+        break;
+      case 'coordinatesDesc':
+        filteredMeasurements = [...allMeasurements].sort((a, b) => calculateDistance(mapCenter, b.location.coordinates) - calculateDistance(mapCenter, a.location.coordinates));
+        break;
+      default:
+        filteredMeasurements = allMeasurements;
+    }
+    setMeasurements(filteredMeasurements.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage));
   };
 
   // Fetching measurements on page load
   useEffect(() => {
-    const loadMeasurements = async () => {
-      const currentMeasurements = await fetchMeasurements(currentPage);
-      setMeasurements(currentMeasurements);
-      const nextMeasurements = await fetchMeasurements(currentPage + 1);
-      setPreloadedMeasurements(nextMeasurements);
-    };
-    loadMeasurements();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage]);
+    fetchMeasurements();
+  }, []);
+
+  // Apply filter whenever filterType, allMeasurements or currentPage changes
+  useEffect(() => {
+    handleFilter();
+  }, [filterType, allMeasurements, currentPage]);
 
   return (
     <div className="blueBackground">
       <h2 className="geolocationTitle">Geolocation Page</h2>
       <div className="geolocationLayout">
-        <MapContainer center={mapCenter} zoom={13} className="measurementMap">
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      <MapContainer center={mapCenter} zoom={13} className="measurementMap">
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {layout === 'points' && measurements.map((measurement, index) => (
+          <MeasurementMarker
+            key={index}
+            measurement={measurement}
+            index={index}
           />
-          {measurements.map((measurement, index) => (
-            <MeasurementMarker
-              key={index}
-              measurement={measurement}
-              index={index}
-            />
-          ))}
-        </MapContainer>
-        <div className="measurementContainer">
-        <h2 className="measurementTitle">Measurement list</h2>
-        <div className="measurementButtonContainer">
-            <button onClick={prevPage} disabled={currentPage === 1}>Previous Page</button>
-            <button onClick={nextPage}>Next Page</button>
+        ))}
+        {layout === 'grid' && (
+          <HeatmapLayer
+            fitBoundsOnLoad
+            fitBoundsOnUpdate
+            points={measurements}
+            longitudeExtractor={m => m.location.coordinates[0]}
+            latitudeExtractor={m => m.location.coordinates[1]}
+            intensityExtractor={m => parseFloat(m.value)}
+          />
+        )}
+      </MapContainer>
+      <div className="measurementContainer">
+        <div className="layoutButtons">
+          <button onClick={() => setLayout('points')}>Points</button>
+          <button onClick={() => setLayout('grid')}>Grid</button>
+        </div>
+        <h2 className="measurementTitle">Measurement List</h2>
+          <div className="measurementButtonContainer">
+            <button onClick={() => prevPage()} disabled={currentPage === 1}>Previous Page</button>
+            <p>Showing page {currentPage} out of {totalPages}</p>
+            <button onClick={() => nextPage()}>Next Page</button>
+            <input type="number" min="1" max={totalPages} onChange={(e) => goToPage(Number(e.target.value))} />
+            <select onChange={(e) => setFilterType(e.target.value)}>
+              <option value="dateAsc">Date Ascending</option>
+              <option value="dateDesc">Date Descending</option>
+              <option value="coordinatesAsc">Coordinates Ascending</option>
+              <option value="coordinatesDesc">Coordinates Descending</option>
+            </select>
           </div>
           <div className="measurementList">
             {measurements.map((measurement, index) => (
@@ -174,8 +155,8 @@ const Geolocation = () => {
           </div>
         </div>
       </div>
-    <div className="geolocationBackground"></div>
-  </div>
+      <div className="geolocationBackground"></div>
+    </div>
   );
 };
 
