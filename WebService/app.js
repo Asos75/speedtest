7d7a793c81989a2a47a85fe2f91dcce01647eed8
Asos75 <andraz.sosteric@gmail.com>
2024-06-02 18:53:13 +0200

    disabled cors

--- WebService/app.js
+++ WebService/app.js
@@ -70,7 +70,7 @@ io.on('connection', (socket) => {
 
 
 app.use(logger('dev'));
-app.use(cors({ origin: 'http://localhost:3000' }));
+//app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
