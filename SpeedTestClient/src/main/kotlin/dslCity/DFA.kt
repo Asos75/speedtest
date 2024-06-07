2da3133bbdbdf42d36962781e978627bef8cb791
Asos75 <andraz.sosteric@gmail.com>
2024-06-07 11:20:16 +0200

    SPEED-18 Added DSLCity to compiler

---
+++ SpeedTestClient/src/main/kotlin/dslCity/DFA.kt
@@ -0,0 +1,10 @@
+package dslCity
+
+interface DFA {
+    val states: Set<Int>
+    val alphabet: IntRange
+    fun next(state: Int, code: Int): Int
+    fun symbol(state: Int): Symbol
+    val startState: Int
+    val finalStates: Set<Int>
+}
